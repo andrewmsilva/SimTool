3c0dff089b236c24abba7c75a5dcbf70db5fd929
Andrew M. Silva <andrewsaxx@gmail.com>
2020-10-26 14:21:20 -0300

    feat(Enviroment): initializing class

--- src/SimTool.py
+++ src/SimTool.py
@@ -1,2 +1,17 @@
 from src.modules.Generator import Generator
+
+class Enviroment(object):
+    __generator = None
+    __currentTime = 0
+
+    def createGenerator(self, *args):
+        self.__generator = Generator(*args)
+    
+    def run(self, until):
+        self.__currentTime = 0
         
+        generations = 0
+        while until == None or generations < until:
+            generations += 1
+            self.__currentTime = self.__generator.new(self.__currentTime)
+            print(self.__currentTime)
