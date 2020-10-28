873b08fc9d56c8ceaf9ba8df7527b35ee1499934
Andrew M. Silva <andrewsaxx@gmail.com>
2020-10-27 22:22:24 -0300

    test(Service): creatind one

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from src.SimTool import Enviroment
 
 env = Enviroment()
-env.createGenerator('Test', 'test', (0, 8), 'normal')
+env.createService('Recepção', (4, 8), num_servers=2, distribution='normal')
+env.createGenerator('Pacientes', 'Recepção', (0, 8), distribution='normal')
 env.run(5)
