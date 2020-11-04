6e953a54bb89e57247aa721ab68dee06a926ea15
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-04 16:54:27 -0300

    bug: fixing double declaration

--- src/modules/RandomRange.py
+++ src/modules/RandomRange.py
@@ -2,10 +2,18 @@ import numpy as np
 
 class RandomRange(object):
     
-    def __init__(self, range, distribution='uniform'):
-        if not (isinstance(range, tuple) and all(isinstance(value, int) for value in range) and range[0] < range[1]):
-            raise ValueError('Range must be a tuple containing two int values, where A < B, A >= 0, and B > 1')
-        self.__range = range
+    def __init__(self, min, max, distribution='uniform'):
+        if not (isinstance(min, int)):
+            raise ValueError('Min value must be integer')
+            
+        if not (isinstance(max, int)):
+            raise ValueError('Max value must be integer')
+
+        if not (min < max):
+            raise ValueError('Min value must be lower than max value')
+
+        self.__min = min
+        self.__max = max
 
         if not (distribution == 'uniform' or distribution == 'normal'):
             raise ValueError('Distribution must be "uniform" or "normal"')
@@ -14,11 +22,11 @@ class RandomRange(object):
     def getNumber(self):
         num = -1
 
-        while num < self.__range[0] or num > self.__range[1]:
+        while num < self.__min or num > self.__max:
             if self.__distribution == 'uniform':
-                num = np.random.uniform(self.__range[0], self.__range[1])
+                num = np.random.uniform(self.__min, self.__max)
             elif self.__distribution == 'normal':
-                range = range(self.__range[0], self.__range[1]+1)
+                range = range(self.__min, self.__max+1)
                 num = np.random.normal(np.mean(range), np.std(range))
         
         return int(num)
