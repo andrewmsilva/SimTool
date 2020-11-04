dc60f5e391b3d31b4dcdc8ffd935ac2bc49a38fd
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-04 17:04:06 -0300

    test: generating some entities

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from src.SimTool import Enviroment
 
 env = Enviroment()
-env.createService('Recepção', (4, 8), num_servers=2, distribution='normal')
-env.createGenerator('Pacientes', 'Recepção', (0, 8), max_generation=60, distribution='normal')
-env.run()
+env.createService('Recepção', "None", (4, 8), num_servers=2, distribution='normal')
+env.createGenerator('Pacientes', 'Recepção', (1, 8), distribution='normal')
+env.run(stop_at=60)
