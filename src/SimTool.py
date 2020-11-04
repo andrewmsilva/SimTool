d42cb5cd524069b1ae193c8ef55ff3c6cac6fea2
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-04 17:05:49 -0300

    feat(enviroment): running with counting time

--- src/SimTool.py
+++ src/SimTool.py
@@ -17,9 +17,16 @@ class Enviroment(object):
             if service.getName() == name:
                 return service
     
-    def run(self):
-        self.__currentTime = 0
+    def __runGenerators(self):
         for generator in self.__generators:
-            entities = generator.entities(self.__currentTime)
-            service = self.__findServiceByName(generator.getDestination())
-            service.addToQueue(entities)
+            if generator.getNext(self.__currentTime):
+                service = self.__findServiceByName(generator.getDestination())
+                service.addToQueue(self.__currentTime)
+                print(service.getQueue())
+    
+    def run(self, stop_at=None, stop_until=None):
+        self.__currentTime = 0
+        
+        while (stop_at == None or self.__currentTime <= stop_at):
+            self.__runGenerators()
+            self.__currentTime += 1
