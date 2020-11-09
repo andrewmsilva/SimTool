85f997d75046757d95c45d6a3d69dd66715b8c7c
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-09 10:36:29 -0300

    feat(service): using Interim class

--- src/modules/Service.py
+++ src/modules/Service.py
@@ -1,69 +1,64 @@
-from src.modules.RandomRange import RandomRange
+from src.modules.Random import Random
+from src.modules.Server import Server
+from src.modules.Interim import Interim
 
-class Attendance(object):
-
-    def __init__(self, start, end, duration):
-        if not (isinstance(start, (int, float))):
-            raise ValueError('Start time must be int or float')
-        self.__start = start
-
-        if not (isinstance(end, (int, float))):
-            raise ValueError('End time must be int or float')
-        self.__end = end
-
-        if not (isinstance(duration, (int, float))):
-            raise ValueError('Duration time must be int or float')
-        self.__duration = duration
+class Service(Random):
     
-    def getStart(self):
-        return self.__start
-    
-    def getEnd(self):
-        return self.__end
-    
-    def getDuration(self):
-        return self.__duration
+    def __init__(self, name, target, min_range, max_range, distribution, num_servers=1, discipline='FCFS'):
+        super(Service, self).__init__(min_range, max_range, distribution)
 
-class Server(object):
-    __events = []
-        
-
-class Service(object):
-    __queue = []
-
-    def __init__(self, name, destination, duration_range, num_servers=1, discipline='FCFS', distribution='uniform'):
         if not (isinstance(name, str)):
             raise ValueError('Name must be a string')
         self.__name = name
 
-        if not (isinstance(destination, str)):
-            raise ValueError('Destination must be a string')
-        self.__destination = destination
+        if not (isinstance(target, str)):
+            raise ValueError('Target must be a string')
+        self.__target = target
 
         if not (isinstance(num_servers, int) and num_servers > 0):
             raise ValueError('Number of num_servers must be an integer greater than 0')
         self.__numServers = num_servers
+        self.__initServers()
         
         if not (discipline == 'FCFS' or discipline == 'LCFS'):
             raise ValueError('Queue discipline must be "FCFS" or "LCFS"')
         self.__discipline = discipline
 
-        self.__randomDuration = RandomRange(duration_range[0], duration_range[1], distribution)
-    
+        self.__input = []
+        self.__output = []
+
     def __initServers(self):
         self.__servers = [ Server() for i in range(self.__numServers) ]
     
     def getName(self):
         return self.__name
     
-    def getDestination(self):
-        return self.__destination
+    def getTarget(self):
+        return self.__target
     
-    def addToQueue(self, entity):
-        self.__queue.append(entity)
+    def inputInterim(self, interim):
+        if not (isinstance(interim, Interim)):
+            raise ValueError('Only Interim objects can be added to queue')
+        self.__input.append(interim)
     
     def getQueue(self):
-        return self.__queue
+        return self.__input
     
-    def attend(self, start):
-        pass
+    def __getNextInterim(self):
+        if self.__discipline == 'FCFS':
+            return self.__input.pop(0)
+        elif self.__discipline == 'LCFS':
+            return self.__input.pop()
+    
+    def attend(self, current_time):
+        for i in range(self.__numServers):
+            server = self.__servers[i]
+            if len(self.__input) > 0:
+                if not server.busy(current_time):
+                    interim = self.__getNextInterim()
+                    duration = self.getRandomNumber()
+                    server.attend(interim, current_time, duration)
+                    print(self.__name+':', interim.getName(), 'arrived at', interim.getTime(), 'and attended by server', i, 'from', current_time, 'to', current_time+duration)
+
+                    #interim.appendEvent(current_time, duration)
+                    self.__output.append(interim)
