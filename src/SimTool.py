8218cd562ec030cc1f5711ed5bdd790c284628cc
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-09 10:37:46 -0300

    feat(SimTool): using Interim

--- src/SimTool.py
+++ src/SimTool.py
@@ -19,14 +19,19 @@ class Enviroment(object):
     
     def __runGenerators(self):
         for generator in self.__generators:
-            if generator.getNext(self.__currentTime):
-                service = self.__findServiceByName(generator.getDestination())
-                service.addToQueue(self.__currentTime)
-                print(service.getQueue())
+            interim = generator.getNext(self.__currentTime)
+            if interim:
+                service = self.__findServiceByName(generator.getTarget())
+                service.inputInterim(interim)
+    
+    def __runServices(self):
+        for service in self.__services:
+            service.attend(self.__currentTime)
     
     def run(self, stop_at=None, stop_until=None):
         self.__currentTime = 0
         
         while (stop_at == None or self.__currentTime <= stop_at):
             self.__runGenerators()
+            self.__runServices()
             self.__currentTime += 1
