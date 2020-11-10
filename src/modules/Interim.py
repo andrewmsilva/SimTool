b96c0a664b4d58174f675cbd7b5f5eb29dbf2886
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-10 17:53:21 -0300

    feat(Interim): adding getter for last service duration

--- src/modules/Interim.py
+++ src/modules/Interim.py
@@ -18,5 +18,9 @@ class Interim(object):
         last_event = self.__events[-1]
         return last_event.getEnd()
     
+    def getLastDuration(self):
+        last_event = self.__events[-1]
+        return last_event.getDuration()
+    
     def appendEvent(self, component_name, current_time, duration):
         self.__events.append(Event(component_name, current_time, current_time+duration, duration))
