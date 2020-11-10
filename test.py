4535eed255c3fcb04924339c4460425a1b06f13f
Andrew M. Silva <andrewsaxx@gmail.com>
2020-11-10 09:34:46 -0300

    test: updating testing model

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from src.SimTool import Enviroment
 
 env = Enviroment()
-env.createService('Recepção', 'Médicos', 3, 7, 'normal', 2)
 env.createGenerator('Paciente', 'Recepção', 1, 8, 'normal')
-env.run(stop_at=60)
+env.createService('Recepção', 'Médicos', 3, 7, 'normal', 2)
+env.createService('Médicos', 'Fim', 8, 14, 'normal', 3)
+env.createTerminator('Fim')
+env.run(stop_at=40)
