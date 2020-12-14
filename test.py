5b2ec117ba98971040129ea79026509bd7098826
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-14 11:15:55 -0300

    test: changing example

--- test.py
+++ test.py
@@ -1,8 +1,35 @@
 from src.SimTool import Enviroment
 
 env = Enviroment()
-env.createGenerator('Paciente', 'Recepção', 1, 8, 'normal')
-env.createService('Recepção', 'Médicos', 3, 7, 'normal', 2)
-env.createService('Médicos', 'Fim', 8, 14, 'normal', 3)
-env.createTerminator('Fim')
-env.run(stop_at=40)
+
+env.createGenerator(
+    name='chegada',
+    target='recepção',
+    min_range=2,
+    max_range=5,
+    distribution='normal',
+    entity_name='paciente'
+)
+
+env.createProcess(
+    name='recepção',
+    target='consulta',
+    min_range=7,
+    max_range=12,
+    distribution='normal',
+    num_resources=2,
+    resource_name='atendente'
+)
+
+env.createProcess(
+    name='consulta',
+    target='saída',
+    min_range=15,
+    max_range=25,
+    distribution='normal',
+    resource_name='médico'
+)
+
+env.createTerminator(name='saída')
+
+env.run(stop_at=30)
