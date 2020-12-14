123ce16fd5e39fa368a27e33b35d82fcc9d1b73a
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-14 11:56:27 -0300

    test: changing names from Portuguese to English

--- test.py
+++ test.py
@@ -3,39 +3,39 @@ from src.SimTool import Model
 model = Model()
 
 model.createGenerator(
-    name='chegada',
-    target='recepção',
+    name='Begin',
+    target='Reception',
     min_range=2,
     max_range=5,
     distribution='normal',
-    entity_name='paciente'
+    entity_name='Patient'
 )
 
 model.createProcess(
-    name='recepção',
-    target='roteador',
+    name='Reception',
+    target='Router',
     min_range=7,
     max_range=12,
     distribution='normal',
     num_resources=2,
-    resource_name='atendente'
+    resource_name='Receptionist'
 )
 
 model.createRouter(
-    name='roteador',
-    targets=['consulta', 'saída'],
+    name='Router',
+    targets=['Medical', 'End'],
     distribution='uniform'
 )
 
 model.createProcess(
-    name='consulta',
-    target='recepção',
+    name='Medical',
+    target='Reception',
     min_range=15,
     max_range=25,
     distribution='normal',
-    resource_name='médico'
+    resource_name='Doctor'
 )
 
-model.createTerminator(name='saída')
+model.createTerminator(name='End')
 
 model.run(stop_at=30)
