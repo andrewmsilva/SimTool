5fd2993990f96411e3e9d976071473664b096163
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-14 11:09:10 -0300

    refactor(Event): removing error reises

--- src/modules/Event.py
+++ src/modules/Event.py
@@ -1,20 +1,9 @@
 class Event(object):
 
     def __init__(self, interim_name, start, end, duration):
-        if not (isinstance(interim_name, str)):
-            raise ValueError('Interim name must be a string')
         self.__interimName = interim_name
-        
-        if not (isinstance(start, int)):
-            raise ValueError('Start time must be an integer')
         self.__start = start
-
-        if not (isinstance(end, int)):
-            raise ValueError('End time must be an integer')
         self.__end = end
-
-        if not (isinstance(duration, int)):
-            raise ValueError('Duration time must be an integer')
         self.__duration = duration
     
     def getInterimName(self):
