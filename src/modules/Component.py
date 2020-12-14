b80f8e9570ba06101d3311263b80dfc828763eee
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-14 11:10:26 -0300

    feat(Component): creating main class for components

---
+++ src/modules/Component.py
@@ -0,0 +1,23 @@
+from src.modules.Random import Random
+
+class Component(object):
+
+    def __init__(self, name, target=None):
+        self.__name = name
+        self.__target = target
+        self.__random = None
+    
+    def getName(self):
+        return self.__name
+    
+    def getTarget(self):
+        return self.__target
+    
+    def setRandom(self, min_range, max_range, distribution):
+        self.__random = Random(min_range, max_range, distribution)
+    
+    def getRandomNumber(self):
+        return self.__random.getRandomNumber()
+
+    def printLog(self, *args):
+        print(self.__name+':', *args)
