e62397b6b79f26d9378aa9f8dfc259231c8de5d4
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-20 21:30:17 -0300

    test: model loading and saving

--- test.py
+++ test.py
@@ -1,41 +1,47 @@
 from src.SimTool import Model
 
-model = Model(stop_time=30)
+try:
+    model = Model.load('model.csv')
+except:
+    model = Model(
+        stop_time=30,
+    )
+    
+    model.createGenerator(
+        name='Begin',
+        target='Reception',
+        min_range=2,
+        max_range=5,
+        distribution='normal',
+        entity_name='Patient'
+    )
 
-model.createGenerator(
-    name='Begin',
-    target='Reception',
-    min_range=2,
-    max_range=5,
-    distribution='normal',
-    entity_name='Patient'
-)
+    model.createProcess(
+        name='Reception',
+        target='Decision',
+        min_range=7,
+        max_range=12,
+        distribution='normal',
+        num_resources=2,
+        resource_name='Receptionist'
+    )
 
-model.createProcess(
-    name='Reception',
-    target='Decision',
-    min_range=7,
-    max_range=12,
-    distribution='normal',
-    num_resources=2,
-    resource_name='Receptionist'
-)
+    model.createRouter(
+        name='Decision',
+        targets=['Medical', 'End']
+    )
 
-model.createRouter(
-    name='Decision',
-    targets=['Medical', 'End']
-)
+    model.createProcess(
+        name='Medical',
+        target='Reception',
+        min_range=15,
+        max_range=25,
+        distribution='normal',
+        resource_name='Doctor'
+    )
 
-model.createProcess(
-    name='Medical',
-    target='Reception',
-    min_range=15,
-    max_range=25,
-    distribution='normal',
-    resource_name='Doctor'
-)
+    model.createTerminator(name='End')
 
-model.createTerminator(name='End')
+    model.save('model.csv')
 
-model.save('model.csv')
 model.run()
