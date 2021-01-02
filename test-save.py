d9c9ac922038d0ffde95ed95d7f498fc7dcee288
Andrew M. Silva <andrewsaxx@gmail.com>
2021-01-01 23:37:49 -0300

    refactor(test-save): testing model creation, saving, and running

---
+++ test-save.py
@@ -0,0 +1,38 @@
+from src.SimTool import Model
+
+model = Model()
+
+model.createGenerator(
+    name='Begin',
+    target='Reception',
+    min_range=3,
+    max_range=8,
+    distribution='normal',
+    max_entities=10,
+    entity_name='Patient'
+)
+
+model.createProcess(
+    name='Reception',
+    target='Medical',
+    min_range=8,
+    max_range=15,
+    distribution='normal',
+    num_resources=2,
+    resource_name='Receptionist'
+)
+
+model.createProcess(
+    name='Medical',
+    target='End',
+    min_range=15,
+    max_range=25,
+    distribution='normal',
+    num_resources=3,
+    resource_name='Doctor'
+)
+
+model.createTerminator(name='End')
+
+model.save('model.csv')
+model.run()
