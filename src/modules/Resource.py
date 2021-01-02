62285db6fe6f94d34d1509fa3181092532ae275d
Andrew M. Silva <andrewsaxx@gmail.com>
2021-01-02 12:18:42 -0300

    feat(Resource): creating a never busy option

--- src/modules/Resource.py
+++ src/modules/Resource.py
@@ -2,8 +2,9 @@ from src.modules.Event import Event
 
 class Resource(object):
 
-    def __init__(self, name):
+    def __init__(self, name, never_busy=False):
         self.__name = name
+        self.__neverBusy = never_busy
         self.__events = []
     
     @property
@@ -19,12 +20,16 @@ class Resource(object):
             self.__events.append(Event(entity.name, current_time, current_time+duration, duration))
 
     def busy(self, current_time):
+        if self.__neverBusy:
+            return False
         last_event = None
         if len(self.__events) > 0:
             last_event = self.__events[-1]
         return not (last_event == None or last_event.end <= current_time)
 
     def idleness(self, end_time):
+        if self.__neverBusy:
+            return None
         idleness = 0
         last_time = 0
         for event in self.__events:
