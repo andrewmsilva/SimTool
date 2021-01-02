474c6887a5d2f26b64bc0f038891c2f6f896a0c3
Andrew M. Silva <andrewsaxx@gmail.com>
2021-01-01 23:26:40 -0300

    feat(Terminator): creating reset method

--- src/modules/Terminator.py
+++ src/modules/Terminator.py
@@ -4,12 +4,18 @@ class Terminator(Component):
 
     def __init__(self, name):
         super(Terminator, self).__init__(name)
+        self.reset()
 
-        self.__entities = []
+    def reset(self):
+        self.__output = []
+    
+    @property
+    def output(self):
+        return self.__output
     
     def receiveEntity(self, entity):
         self.printLog('{}: {} finished the simulation at {}'.format(self.name, entity.name, entity.currentTime))
-        self.__entities.append(entity)
+        self.__output.append(entity)
     
     def saveMe(self, writer, columns):
         row = { key: None for key in columns }
